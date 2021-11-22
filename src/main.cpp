79ce5836c0a36fb0cb55450c469ae2ca03def34b
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2021-11-22 22:23:48 +0000

    Remove test code

--- src/main.cpp
+++ src/main.cpp
@@ -16,29 +16,13 @@
 #include "player.h"
 
 std::unique_ptr<Renderer> renderer_;
-std::unique_ptr<Shader> model_shader_;
-std::unique_ptr<Shader> animated_model_shader_;
 std::unique_ptr<Camera> camera_;
-std::vector<AnimatedModel> models_;
-std::unique_ptr<Player> player_;
 
 void InitialiseScene() {
   renderer_->Init();
 
   renderer_ = std::make_unique<Renderer>();
   camera_ = std::make_unique<Camera>();
-  model_shader_ = std::make_unique<Shader>("shaders/model_shader.vs",
-                                           "shaders/model_shader.fs");
-
-  animated_model_shader_ = std::make_unique<Shader>(
-      "shaders/animated_model_shader.vs", "shaders/model_shader.fs");
-
-  // models_.push_back(AnimatedModel("models/rabbit/body.fbx",
-  //                                *animated_model_shader_, *renderer_));
-  // models_.push_back(AnimatedModel("models/polar_bear/body.fbx",
-  //                                *animated_model_shader_, *renderer_));
-
-  player_ = std::make_unique<Player>(*animated_model_shader_, *renderer_);
 }
 
 void Display() {
@@ -55,46 +39,6 @@ void Display() {
   persp_proj_mat = glm::perspective<float>(45.0f, (float)width / (float)height,
                                            1.0f, 1000.0f);
 
-  model_shader_->Bind();
-  model_shader_->SetUniformMatrix4fv("view", GL_FALSE,
-                                     glm::value_ptr(view_mat));
-  model_shader_->SetUniformMatrix4fv("proj", GL_FALSE,
-                                     glm::value_ptr(persp_proj_mat));
-  model_shader_->SetUniformMatrix4fv("model", GL_FALSE,
-                                     glm::value_ptr(model_mat));
-
-  camera_->UpdatePosition();
-
-  animated_model_shader_->Bind();
-  animated_model_shader_->SetUniformMatrix4fv("view", GL_FALSE,
-                                              glm::value_ptr(view_mat));
-  animated_model_shader_->SetUniformMatrix4fv("proj", GL_FALSE,
-                                              glm::value_ptr(persp_proj_mat));
-  animated_model_shader_->SetUniformMatrix4fv("model", GL_FALSE,
-                                              glm::value_ptr(model_mat));
-
-  for (unsigned int i = 0; i < models_.size(); i++) {
-    AnimatedModel& model = models_[i];
-
-    model.UpdateBoneTransformations();
-    const std::vector<glm::mat4>& transforms = model.GetFinalTransforms();
-
-    animated_model_shader_->SetUniformMatrix4fv(
-        "final_bone_transforms", GL_FALSE, glm::value_ptr(transforms.front()),
-        (GLsizei)transforms.size());
-
-    model_mat =
-        glm::translate(glm::mat4(1.0f), glm::vec3(5.0f * i, 0.0f, 0.0f));
-
-    animated_model_shader_->SetUniformMatrix4fv("model", GL_FALSE,
-                                                glm::value_ptr(model_mat));
-
-    model.Draw();
-  }
-
-  player_->UpdatePosition();
-  player_->Draw();
-
   glutSwapBuffers();
 }
 
@@ -108,12 +52,6 @@ void UpdateScene() {
 
   camera_->SetDelta(delta);
 
-  for (AnimatedModel& model : models_) {
-    model.SetDelta(delta);
-  }
-
-  player_->SetDelta(delta);
-
   // Draw the next frame
   glutPostRedisplay();
 }
@@ -126,12 +64,10 @@ void OnKeyboardDown(unsigned char key, int x, int y) {
   }
 
   camera_->OnKeyboardDown(key);
-  player_->OnKeyboardDown(key);
 }
 
 void OnKeyboardUp(unsigned char key, int x, int y) {
   camera_->OnKeyboardUp(key);
-  player_->OnKeyboardUp(key);
 }
 
 int main(int argc, char** argv) {
